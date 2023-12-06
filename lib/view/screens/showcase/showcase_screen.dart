e5a3210c35fef214134d333b39892e7a9f1ea944
kumarsaravana001 <saravanakm46@gmail.com>
2023-12-06 15:52:46 +0530

    updated questions

--- lib/view/screens/showcase/showcase_screen.dart
+++ lib/view/screens/showcase/showcase_screen.dart
@@ -22,105 +22,110 @@ class _ShowCaseScreenState extends State<ShowCaseScreen> {
   int currentQuestionIndex = 0;
   List<Map<String, dynamic>> quizData = [
     {
-      'question':
-          'What financial statement lists assets from current to long term?',
+      'question': 'What does ROI stand for in finance?',
       'options': [
-        'Balance sheet',
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings'
+        'Return on Investment',
+        'Rate of Income',
+        'Risk of Inflation',
+        'Revenue of Interest'
       ],
       'correctAnswerIndex': 0,
     },
     {
-      'question': 'What financial statement lists ?',
+      'question':
+          'Which market index is often used as a barometer for the performance of the U.S. stock market?',
       'options': [
-        'Statement of retainer earnings',
-        'Balance sheet',
-        'Income Statement',
-        'Cashflow statement',
+        'Nikkei Index',
+        'Nikkei Index',
+        'FTSE 100 Index',
+        'S&P 500 Index',
+      ],
+      'correctAnswerIndex': 3,
+    },
+    {
+      'question': 'What is the primary function of a central bank?',
+      'options': [
+        'Regulating commercial banks',
+        'Issuing currency',
+        'Managing monetary policy',
+        'All of the above',
+      ],
+      'correctAnswerIndex': 2,
+    },
+    {
+      'question': 'What does the term "Bull Market" refer to in finance?',
+      'options': [
+        'A market characterized by falling prices',
+        'A market characterized by stagnation',
+        'A market characterized by rising prices',
+        'A market characterized by high volatility',
+      ],
+      'correctAnswerIndex': 2,
+    },
+    {
+      'question':
+          'What is the primary function of red blood cells in the human body?',
+      'options': [
+        'Transportation of oxygen',
+        'Fighting infections',
+        'Digesting nutrients',
+        'Storing energy',
+      ],
+      'correctAnswerIndex': 0,
+    },
+    {
+      'question':
+          'Which organ is responsible for filtering and detoxifying blood in the human body?',
+      'options': [
+        'Kidneys',
+        'Liver',
+        'Heart',
+        'Lungs',
+      ],
+      'correctAnswerIndex': 0,
+    },
+    {
+      'question':
+          'What is the recommended daily intake of water for the average adult?',
+      'options': [
+        '1 liter',
+        '2 liters',
+        '3 liters',
+        '4 liters',
       ],
       'correctAnswerIndex': 1,
     },
     {
-      'question': 'What  lists ?',
+      'question':
+          'Which vitamin is essential for the absorption of calcium in the body?',
       'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
+        'Vitamin A',
+        'Vitamin B12',
+        'Vitamin D',
+        'Vitamin C',
       ],
       'correctAnswerIndex': 2,
     },
     {
-      'question': 'What  lists ?',
+      'question':
+          'What is the purpose of the respiratory system in the human body?',
       'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
+        'Pumping blood',
+        'Digesting food',
+        'Exchanging gasses',
+        'Filtering toxins',
       ],
       'correctAnswerIndex': 2,
     },
     {
-      'question': 'What  lists ?',
+      'question': 'In which country did the sport of archery originate?',
       'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
+        'Greece',
+        'China ',
+        'Egypt',
+        'Brazil',
       ],
-      'correctAnswerIndex': 2,
-    },
-    {
-      'question': 'What  lists ?',
-      'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
-      ],
-      'correctAnswerIndex': 2,
-    },
-    {
-      'question': 'What  lists ?',
-      'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
-      ],
-      'correctAnswerIndex': 2,
-    },
-    {
-      'question': 'What  lists ?',
-      'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
-      ],
-      'correctAnswerIndex': 2,
-    },
-    {
-      'question': 'What  lists ?',
-      'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
-      ],
-      'correctAnswerIndex': 2,
-    },
-    {
-      'question': 'What  lists ?',
-      'options': [
-        'Income Statement',
-        'Cashflow statement',
-        'Statement of retainer earnings',
-        'Balance sheet',
-      ],
-      'correctAnswerIndex': 2,
+      'correctAnswerIndex': 1,
     },
   ];
   void handleOptionTap(int optionIndex) {
