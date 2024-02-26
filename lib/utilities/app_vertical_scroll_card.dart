be459685a626a1f32bd108671f9792344889ff8e
kumarsaravana001 <saravanakm46@gmail.com>
2024-02-26 11:59:06 +0530

    playlist

--- lib/utilities/app_vertical_scroll_card.dart
+++ lib/utilities/app_vertical_scroll_card.dart
@@ -67,3 +67,104 @@ class AppVerticalScrollCard extends StatelessWidget {
     );
   }
 }
+
+class AppVerticalScrollCard1 extends StatelessWidget {
+  final List<String>? titleCard;
+  final List<String>? subtitleCard;
+  final List<Widget> imageList;
+  final int imageListCount;
+  final Function(String showId) onTap;
+  final List<String>? showIds;
+
+  const AppVerticalScrollCard1({
+    super.key,
+    required this.titleCard,
+    this.subtitleCard,
+    required this.imageList,
+    required this.imageListCount,
+    required this.onTap,
+    this.showIds,
+  });
+  String _truncateTitle(String title) {
+    List<String> words = title.split(' ');
+    if (words.length > 1) {
+      return '${words[0]} ...';
+    } else {
+      return title;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          mainAxisExtent: 200),
+      itemCount: imageListCount,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            if (showIds != null && index < showIds!.length) {
+              onTap(showIds![index]);
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                color: AppColors.black,
+                borderRadius: BorderRadius.circular(15.0)),
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.sp, horizontal: 14.sp),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(titleCard![index],
+                              style: AppTestStyle.headingint(
+                                  fontSize: 13.sp,
+                                  color: AppColors.white,
+                                  fontWeight: FontWeight.bold)),
+                          Text(subtitleCard![index],
+                              style: AppTestStyle.headingint(
+                                  fontSize: 16.sp,
+                                  color: AppColors.white,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                      // Assuming you want to show some static text or you can modify it to use list values
+                      Text("Coming soon",
+                          style: AppTestStyle.headingint(
+                              fontSize: 15.sp,
+                              color: AppColors.white,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  // Assuming eventsImagepath is a widget that takes a string (image path)
+                  // and you have an equivalent widget or method to display images
+                  child: imageList[index],
+                ),
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      bottomRight: Radius.circular(15)),
+                  child: Container(
+                    height: 10,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/view/screens/playlist/playlist_screen.dart
+++ lib/view/screens/playlist/playlist_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ondgo_flutter/bloc/showscreen_bloc/showid_cubit.dart';
 import 'package:ondgo_flutter/config/config_index.dart';
 import 'package:ondgo_flutter/utilities/app_banner_list.dart';
+import 'package:ondgo_flutter/view/screens/homescreen/widgets/widget.dart';
+import '../../../bloc/homescreen_bloc/popular_picks_bloc/popular_picks_bloc.dart';
+import '../../../bloc/homescreen_bloc/popular_picks_bloc/popular_picks_event.dart';
+import '../../../bloc/homescreen_bloc/popular_picks_bloc/popular_picks_state.dart';
+import '../../../bloc/navigation_cubit/navigationbar_cubit.dart';
+import '../../../bloc/showscreen_bloc/showDetails_bloc/show_details_bloc.dart';
+import '../../../bloc/showscreen_bloc/showDetails_bloc/show_details_event.dart';
+import '../../../bloc/showscreen_bloc/showEpisodeDetails_bloc/showEpisode_details_bloc.dart';
+import '../../../bloc/showscreen_bloc/showEpisodeDetails_bloc/showEpisode_details_event.dart';
 import '../../../utilities/app_horizontal_scroll_card.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../utilities/app_vertical_scroll_card.dart';
 
 class PlayListScreen extends StatefulWidget {
   const PlayListScreen({super.key});
@@ -13,80 +27,195 @@ class PlayListScreen extends StatefulWidget {
 class _PlayListScreenState extends State<PlayListScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children: [
-            Positioned(
-              bottom: -125,
-              left: -10,
-              right: 30,
-              child: SvgPicture.asset(IconAssets.bottombgdiamond),
-            ),
-            Column(
-              children: [
-                SvgPicture.asset(
-                    width: MediaQuery.of(context).size.width,
-                    IconAssets.profilescreenbgblack),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    AppLocalisation.yourplaylist,
-                    style: AppTestStyle.headingBai(fontSize: 26.sp),
-                  ),
-                ),
-                HorizontalScrollableCard(
-                  cardStatusColor: Colors.indigo,
-                  imageListCount: playlistcardnames.length,
-                  imageList: yourlistImagepath,
-                  cardbackgroundcolor: AppColors.black,
-                  textColor: AppColors.white,
-                  showIds: [],
-                  onTap: (String showId) {},
-                  titlecard: [],
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    AppLocalisation.videos,
-                    style: AppTestStyle.headingBai(fontSize: 26.sp),
-                  ),
-                ),
-                HorizontalScrollableCard(
-                  cardStatusColor: Colors.indigo,
-                  imageListCount: playlistcardnames.length,
-                  imageList: yourlistImagepath,
-                  cardbackgroundcolor: AppColors.black,
-                  textColor: AppColors.white,
-                  showIds: [],
-                  onTap: (String showId) {},
-                  titlecard: [],
-                )
-              ],
-            ),
-            Positioned(
-              top: 0,
-              right: 30,
-              child: Row(
-                children: [SvgPicture.asset(IconAssets.badgeopen)],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PopularPicksBloc>(
+            create: (context) => PopularPicksBloc()..add(FetchPopularPicks())),
+      ],
+      child: SafeArea(
+        child: Scaffold(
+          body: Stack(
+            children: [
+              Positioned(
+                bottom: -125,
+                left: -10,
+                right: 30,
+                child: SvgPicture.asset(IconAssets.bottombgdiamond),
               ),
-            ),
-            Positioned(
-              top: 10,
-              left: 20,
-              child: Row(
+              Column(
                 children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Icon(Icons.arrow_back_outlined,
-                        color: AppColors.white, size: 30),
-                  )
+                  SvgPicture.asset(
+                      width: MediaQuery.of(context).size.width,
+                      IconAssets.profilescreenbgblack),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      AppLocalisation.yourplaylist,
+                      style: AppTestStyle.headingBai(fontSize: 26.sp),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.0.sp, top: 30.sp),
+                    child: BlocBuilder<PopularPicksBloc, PopularPicksState>(
+                      builder: (context, state) {
+                        if (state is PopularPicksLoaded &&
+                            state.popularPicks.isNotEmpty) {
+                          List<String> showNames = state.popularPicks
+                              .map((show) => show.showName ?? 'No Show Name')
+                              .toList();
+                          List<String> showIds = state.popularPicks
+                              .map((show) => show.showId ?? 'No Show Name')
+                              .toList();
+                          List<Widget> imageWidgets =
+                              state.popularPicks.map((show) {
+                            String imageUrl = show.thumbnail!.isNotEmpty
+                                ? show.thumbnail![0]
+                                : 'default_image_url';
+
+                            return Image.network(imageUrl, fit: BoxFit.cover);
+                          }).toList();
+
+                          return Column(
+                            children: [
+                              HorizontalScrollableCard(
+                                titlecard: showNames,
+                                imageListCount: state.popularPicks.length,
+                                imageList: imageWidgets,
+                                textColor: AppColors.white,
+                                cardStatusColor: Colors.indigoAccent,
+                                onTap: (String showId) {
+                                  final int parsedShowId =
+                                      int.tryParse(showId) ?? 0;
+                                  context
+                                      .read<ShowIdCubit>()
+                                      .updateShowId(parsedShowId);
+
+                                  BlocProvider.of<NavigationCubit>(context)
+                                      .navigateToIndex(3);
+                                  BlocProvider.of<UserShowDetailBloc>(context)
+                                      .add(FetchUserShowDetail(
+                                          showId: int.parse(showId)));
+                                  BlocProvider.of<UserEpisodeDetailBloc>(
+                                          context)
+                                      .add(FetchUserEpisodeDetail(
+                                          showId: int.parse(showId)));
+                                },
+                                showIds: showIds,
+
+                                //   Expanded(
+                                // child: AppVerticalScrollCard1(
+                                //   titleCard: showNames,
+                                //   imageListCount: state.popularPicks.length,
+                                //   imageList: imageWidgets,
+                                //   onTap: (String showId) {
+                                //     final int parsedShowId =
+                                //         int.tryParse(showId) ?? 0;
+                                //     context
+                                //         .read<ShowIdCubit>()
+                                //         .updateShowId(parsedShowId);
+
+                                //     BlocProvider.of<NavigationCubit>(context)
+                                //         .navigateToIndex(3);
+                                //     BlocProvider.of<UserShowDetailBloc>(context)
+                                //         .add(FetchUserShowDetail(
+                                //             showId: int.parse(showId)));
+                                //     BlocProvider.of<UserEpisodeDetailBloc>(context)
+                                //         .add(FetchUserEpisodeDetail(
+                                //             showId: int.parse(showId)));
+                                //   },
+                                //   showIds: showIds,
+                                // ),
+                                //   )
+                                // ],
+                              ),
+                              HorizontalScrollableCard(
+                                titlecard: showNames,
+                                imageListCount: state.popularPicks.length,
+                                imageList: imageWidgets,
+                                textColor: AppColors.white,
+                                cardStatusColor: Colors.indigoAccent,
+                                onTap: (String showId) {
+                                  final int parsedShowId =
+                                      int.tryParse(showId) ?? 0;
+                                  context
+                                      .read<ShowIdCubit>()
+                                      .updateShowId(parsedShowId);
+
+                                  BlocProvider.of<NavigationCubit>(context)
+                                      .navigateToIndex(3);
+                                  BlocProvider.of<UserShowDetailBloc>(context)
+                                      .add(FetchUserShowDetail(
+                                          showId: int.parse(showId)));
+                                  BlocProvider.of<UserEpisodeDetailBloc>(
+                                          context)
+                                      .add(FetchUserEpisodeDetail(
+                                          showId: int.parse(showId)));
+                                },
+                                showIds: showIds,
+                              )
+                            ],
+                          );
+                        } else if (state is PopularPicksLoading) {
+                          return horizontalCardShimmerWidget();
+                        } else {
+                          return horizontalCardShimmerWidget();
+                        }
+                      },
+                    ),
+                  ),
+                  // HorizontalScrollableCard(
+                  //   cardStatusColor: Colors.indigo,
+                  //   imageListCount: playlistcardnames.length,
+                  //   imageList: yourlistImagepath,
+                  //   cardbackgroundcolor: AppColors.black,
+                  //   textColor: AppColors.white,
+                  //   showIds: [],
+                  //   onTap: (String showId) {},
+                  //   titlecard: [],
+                  // ),
+                  // Align(
+                  //   alignment: Alignment.center,
+                  //   child: Text(
+                  //     AppLocalisation.videos,
+                  //     style: AppTestStyle.headingBai(fontSize: 26.sp),
+                  //   ),
+                  // ),
+                  // HorizontalScrollableCard(
+                  //   cardStatusColor: Colors.indigo,
+                  //   imageListCount: playlistcardnames.length,
+                  //   imageList: yourlistImagepath,
+                  //   cardbackgroundcolor: AppColors.black,
+                  //   textColor: AppColors.white,
+                  //   showIds: [],
+                  //   onTap: (String showId) {},
+                  //   titlecard: [],
+                  // )
                 ],
               ),
-            ),
-          ],
+              Positioned(
+                top: 0,
+                right: 30,
+                child: Row(
+                  children: [SvgPicture.asset(IconAssets.badgeopen)],
+                ),
+              ),
+              Positioned(
+                top: 10,
+                left: 20,
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Icon(Icons.arrow_back_outlined,
+                          color: AppColors.white, size: 30),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
