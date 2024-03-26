e6c42fb41544a6a8c78a895589cd18edef1de691
kumarsaravana001 <saravanakm46@gmail.com>
2024-03-26 10:36:42 +0530

    other commits

--- lib/utilities/app_bottom_navbar.dart
+++ lib/utilities/app_bottom_navbar.dart
@@ -1,101 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:ondgo_flutter/bloc/navigation_cubit/navbar_cubit.dart';
-// import 'package:ondgo_flutter/view/screens/search/search_library_screen.dart';
-// import 'package:ondgo_flutter/view/view_index.dart';
-// import '../../../config/config_index.dart';
-
-// class Navbar extends StatefulWidget {
-//   const Navbar({Key? key}) : super(key: key);
-
-//   @override
-//   State<Navbar> createState() => _NavbarState();
-// }
-
-// class _NavbarState extends State<Navbar> {
-//   int _selectedIndex = 0;
-
-//   void _onItemTapped(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//     });
-//   }
-
-// ignore_for_file: deprecated_member_use
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider<NavigationCubit>(
-//         create: (_) => NavigationCubit(),
-//         child: Scaffold(
-//           body: Stack(
-//             children: [
-//               Positioned.fill(
-//                 child: IndexedStack(
-//                   index: _selectedIndex,
-//                   children: const [
-//                     HomeScreen(),
-//                     SearchandLibraryScreen(),
-//                     ReelsScreen(),
-//                     ShowCaseScreen(),
-//                     ProfileScreen(),
-//                   ],
-//                 ),
-//               ),
-//               Positioned(
-//                 left: 15,
-//                 right: 15,
-//                 bottom: 0,
-//                 child: ClipRRect(
-//                   borderRadius: const BorderRadius.only(
-//                     topLeft: Radius.circular(20),
-//                     topRight: Radius.circular(20),
-//                   ),
-//                   child: BottomNavigationBar(
-//                     backgroundColor: AppColors.black,
-//                     type: BottomNavigationBarType.fixed,
-//                     items: <BottomNavigationBarItem>[
-//                       BottomNavigationBarItem(
-//                         icon: SvgPicture.asset(IconAssets.navbaricon1,
-//                             height: 25),
-//                         label: '',
-//                       ),
-//                       BottomNavigationBarItem(
-//                         icon: SvgPicture.asset(
-//                           IconAssets.horizontaldiamond,
-//                           height: 20,
-//                           color: AppColors.white,
-//                         ),
-//                         label: '',
-//                       ),
-//                       BottomNavigationBarItem(
-//                         icon: SvgPicture.asset(IconAssets.navbaricon2,
-//                             height: 25),
-//                         label: '',
-//                       ),
-//                       BottomNavigationBarItem(
-//                         icon: SvgPicture.asset(IconAssets.navbaricon3,
-//                             height: 25),
-//                         label: '',
-//                       ),
-//                       BottomNavigationBarItem(
-//                         icon: SvgPicture.asset(IconAssets.badgecloseblack,
-//                             height: 48),
-//                         label: '',
-//                       ),
-//                     ],
-//                     currentIndex: _selectedIndex,
-//                     selectedFontSize: 0,
-//                     unselectedFontSize: 0,
-//                     onTap: _onItemTapped,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ));
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
@@ -103,7 +5,10 @@ import 'package:ondgo_flutter/config/app_colors.dart';
 import 'package:ondgo_flutter/config/app_icons.dart';
 import 'package:ondgo_flutter/view/screens/profile/profile_screen.dart';
 import 'package:ondgo_flutter/bloc/navigation_cubit/navigationbar_cubit.dart';
+import 'package:ondgo_flutter/view/screens/reels/reels_full_screen.dart';
 import 'package:ondgo_flutter/view/screens/reels/reels_screen.dart';
+import 'package:ondgo_flutter/view/screens/search/search_library_screen.dart';
+import 'package:ondgo_flutter/view/screens/showcase/showcase_screen.dart';
 import '../view/screens/homescreen/home.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 
@@ -115,6 +20,8 @@ class Navbar extends StatefulWidget {
 }
 
 class _NavbarState extends State<Navbar> {
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+
   final List<BottomNavigationBarItem> _navBarItems = [
     BottomNavigationBarItem(
       icon: SvgPicture.asset(IconAssets.navbaricon1, height: 25),
@@ -143,11 +50,11 @@ class _NavbarState extends State<Navbar> {
     const HomeScreen(),
     const ReelsScreen(),
     const ProfileScreen(),
-    // SearchandLibraryScreen(),
+    // const ShowCaseScreen(),
+    // const SearchandLibraryScreen(),
     // ReelsScreen(),
     // ReelsFullScreen(),
-    // ShortsPage(),
-    // ShowCaseScreen(),
+    // const ShortsPage(),
   ];
   @override
   void initState() {
@@ -159,7 +66,7 @@ class _NavbarState extends State<Navbar> {
     final NavigationCubit navigationCubit =
         BlocProvider.of<NavigationCubit>(context);
     if (navigationCubit.state != 0) {
-      navigationCubit.navigateToIndex(0); 
+      navigationCubit.navigateToIndex(0);
       return true;
     }
     return false;
@@ -172,9 +79,32 @@ class _NavbarState extends State<Navbar> {
         builder: (context, selectedIndex) {
           return Stack(
             children: [
-              IndexedStack(
-                index: selectedIndex,
-                children: _screens,
+              Positioned.fill(
+                child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    const begin = Offset(0.0, 1.0); // Start from the bottom
+                    const end = Offset.zero;
+                    const curve = Curves.easeInOut;
+
+                    var slideTween = Tween(begin: begin, end: end)
+                        .chain(CurveTween(curve: curve));
+                    var fadeTween = Tween(begin: 0.0, end: 1.0); // For fading
+
+                    return FadeTransition(
+                      opacity: animation.drive(fadeTween),
+                      child: SlideTransition(
+                        position: animation.drive(slideTween),
+                        child: child,
+                      ),
+                    );
+                  },
+                  child: IndexedStack(
+                    index: selectedIndex,
+                    children: _screens,
+                  ),
+                ),
               ),
               Positioned(
                 left: 15,
